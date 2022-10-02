3b763b59ef8c74ec053c747cfa4db65404f2f4db
Haruna Popoola <haruna.popoola@yahoo.com>
2022-10-02 23:18:11 +0100

    feature: add related schemas for projects and properties

---
+++ src/api/project-gallery/controllers/project-gallery.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * project-gallery controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::project-gallery.project-gallery');
---
+++ src/api/project-gallery/routes/project-gallery.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * project-gallery router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::project-gallery.project-gallery');
