e19db2447d5e120ddc63d00ab7fb4cac765d9609
Haruna Popoola <haruna.popoola@yahoo.com>
2022-11-03 08:53:01 +0100

    feature: add sendgrid email

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -6,4 +6,16 @@ module.exports = ({ env }) => ({
       jwtSecret: env("JWT_SECRET") || crypto.randomBytes(16).toString("base64"),
     },
   },
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "no-reply@highrahcy.com",
+        defaultReplyTo: "no-reply@highrachy.com",
+      },
+    },
+  },
 });
